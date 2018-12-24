2bcff049610eed844866e3697ecc6f747f08471b
Dmitry Khlystov <dkhlystov@gmail.com>
2018-12-25 03:12:52 +1000

    Add menu and catalog modules

--- config/modules.php
+++ config/modules.php
@@ -1,6 +1,8 @@
 <?php
 
 return [
+    'menu' => 'smart\menu\backend\Module',
     'page' => 'smart\page\backend\Module',
+    'catalog' => 'smart\catalog\backend\Module',
     'user' => 'smart\user\backend\Module',
 ];
