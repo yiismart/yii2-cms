9b4a7db3ebff564282c7980d5f3a30e6b0aa0669
Dmitry Khlystov <dkhlystov@gmail.com>
2019-05-04 17:20:18 +1000

    User module config

--- config/modules.php
+++ config/modules.php
@@ -1,8 +1,5 @@
 <?php
 
 return [
-    'menu' => 'smart\menu\backend\Module',
-    'page' => 'smart\page\backend\Module',
-    'catalog' => 'smart\catalog\backend\Module',
-    'user' => 'smart\user\backend\Module',
+    'user' => 'smart\user\Module',
 ];
