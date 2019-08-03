7c25b29eb40d06bd8911e6ad474e5db924a26c21
Dmitry Khlystov <dkhlystov@gmail.com>
2019-08-04 01:20:45 +1000

    City and catalog modules

--- config/modules.php
+++ config/modules.php
@@ -1,7 +1,9 @@
 <?php
 
 return [
+    'city' => 'smart\city\backend\Module',
     'page' => 'smart\page\backend\Module',
     'news' => 'smart\news\backend\Module',
+    'catalog' => 'smart\catalog\backend\Module',
     'user' => 'smart\user\backend\Module',
 ];
