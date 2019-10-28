d423390a72f26ec0dd9da9c1ec5c8a70801dcc81
Dmitry Khlystov <dkhlystov@gmail.com>
2019-10-28 16:17:26 +0100

    Update modules

--- config/modules.php
+++ config/modules.php
@@ -1,9 +1,15 @@
 <?php
 
 return [
+    'menu' => 'smart\menu\backend\Module',
     'city' => 'smart\city\backend\Module',
     'page' => 'smart\page\backend\Module',
     'news' => 'smart\news\backend\Module',
-    'catalog' => 'smart\catalog\backend\Module',
+    // 'catalog' => 'smart\catalog\backend\Module',
+
+    // 'article' => 'smart\article\backend\Module',
+
+    // 'seo' => 'smart\seo\backend\Module',
     'user' => 'smart\user\backend\Module',
+    'sitemap' => 'smart\sitemap\backend\Module',
 ];
